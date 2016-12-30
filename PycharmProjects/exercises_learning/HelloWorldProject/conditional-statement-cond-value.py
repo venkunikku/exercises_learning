0cdb013bb30354da0f76e18b29d25db60a1d5d83
Nikhila Arigela <arigelanikhila@gmail.com>
2016-12-30 17:00:29 -0600

    delete

--- PycharmProjects/exercises_learning/HelloWorldProject/conditional-statement-cond-value.py
+++
@@ -1,8 +0,0 @@
-
-def cond():
-    a, b = 1, 0
-    s = "a less than" if a < b else 'a is greater than or equal to b'
-    print(s)
-
-if __name__ == '__main__':
-    cond()
--- PycharmProjects/exercises_learning/HelloWorldProject/lambd_test.py
+++
@@ -1,2 +0,0 @@
-def inc(n):
-    return lambda x:x+n

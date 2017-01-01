7eb844f4e6f49d12b0139cc4baa3278e417425eb
venku <venku.buragadda.acc@gmail.com>
2017-01-01 13:50:46 -0600

    ch3 dictionary type and shall copy and formkeys etc

--- PycharmProjects/HelloWorldProject/PythonLearnAgain/ch3/ch2_dictionary_type.py
+++ PycharmProjects/HelloWorldProject/PythonLearnAgain/ch3/ch2_dictionary_type.py
@@ -17,13 +17,28 @@ if __name__ == '__main__':
         print(item)
         print('keys',item.keys())
 
+        print('************Shall Copy******************')
+        #Making a Shall copy of the dictionary
+        shallow_copy_of_dict = item.copy()
+        print('Shall Copy of the dictionary', shallow_copy_of_dict)
+
+        item['newitem'] = 'newvalue into newitem'
+
+        item['array'] = item['array']+[10]
+        print('Oringinal Dic:', item)
+        print('Shallow copy of the main dict: NO CHANGE SINCE WE DID NOT Modify object', shallow_copy_of_dict)
+
+
+        print('************Shall Copy- End******************')
+
         #mapping key to the values
         # it will not effect item dic. It will create a new dict
         s = [1,2,3,4,5,'hello']
-        print(item.fromkeys(s,'same value for all'))
+        print(item.fromkeys(s,'same value for all')) #doing this way gives you a 'item' type to new dict that is created
+        #  from formkeys. This is the main difference between this and the below dict.fromkeys()
 
         # or you can use dict.formkeys()
-        print(dict.fromkeys(s,'same values for all'))
+        print(dict.fromkeys(s,'same values for all')) # this is a classMethod where you can create a new dict.
 
         print(item.items())
         print(type(item.keys()))
